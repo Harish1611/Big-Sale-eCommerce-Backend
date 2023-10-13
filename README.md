cde2d4d99e0c2ecbc4a6628f238e10d6ef97b7b5
Harish1611 <harishnaidub005@gmail.com>
2023-10-13 12:17:56 +0530

    Big Sale - eCommerce: created Basic structure and Established connection with MongoDB

---
+++ README.md
@@ -0,0 +1,10 @@
+# Big Sale - eCommerce Backend
+
+Big Sale - eCommerce Backend is the backend of an e-commerce web application built with MongoDB, NodeJS, and ExpressJS.
+
+## Tools & Technologies
+
+- ReactJS
+- MongoDB
+- NodeJS
+- ExpressJS
---
+++ config.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const URL = process.env.MONGO_URL;
+mongoose.set("strictQuery", true);
+const connectToMongo = async () => {
+  try {
+    let db = await mongoose.connect(URL);
+    console.log(db.connection.host);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connectToMongo;
