a1abb6a95f86d34753d9ee8502741a03d634dab0
Harish1611 <harishnaidub005@gmail.com>
2023-11-20 22:49:27 +0530

    New: Created user controller with get user profile and get all user functions

---
+++ src/controller/user.controller.js
@@ -0,0 +1,33 @@
+const userService = require("../services/user.service")
+
+const getUserProfile = async (req,res) => {
+
+    try{
+
+        const jwt = req.headers.authorization?.split(" ")[1];
+
+        if(!jwt){
+            return res.status(404).send({error:"Token not found"});
+        }
+
+        const user = await userService.getUserProfileByToken(jwt);
+
+        return res.status(200).send(user);
+    }catch(error){
+
+        return res.status(500).send({error:error.message})
+
+    }
+}
+
+const getAllUsers = async(req, res) => {
+    try{
+        const users = await userService.getAllUsers();
+        return res.status(200).send(users);
+
+    }catch (error) {
+        return res.status(500).send({error: error.message})
+    }
+}
+
+module.exports= {getUserProfile, getAllUsers}
