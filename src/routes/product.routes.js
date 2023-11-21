31947e0ce96e0e75f2caf6f9f0398870b1663850
Harish1611 <harishnaidub005@gmail.com>
2023-11-21 17:58:30 +0530

    New: Created Product Router layer with Product  api routes

---
+++ src/routes/product.routes.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const router=express.Router();
+const productController=require("../controller/product.controller.js");
+const authenticate = require("../middleware/authenticate.js");
+
+router.get('/',authenticate, productController.getAllProducts);
+router.get('/id/:id',authenticate, productController.findProductById);
+
+
+module.exports = router;
