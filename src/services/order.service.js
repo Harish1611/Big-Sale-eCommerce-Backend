7bdfa54a3d7d694133e889e00dd7ae6b1834d1a9
Harish1611 <harishnaidub005@gmail.com>
2023-11-21 15:37:34 +0530

    New: Created order service layer with createOrder, placeOrder, confirmedOrder, shipOrder, deliveredOrder, cancelledOrder functions

---
+++ src/services/order.service.js
@@ -0,0 +1,94 @@
+const Address = require("../models/address.model.js");
+const Order = require("../models/order.model.js");
+const OrderItem = require("../models/orderItems.js");
+const cartService = require("../services/cart.service.js");
+
+async function createOrder(user, shippAddress) {
+  let address;
+  if (shippAddress._id) {
+    let existedAddress = await Address.findById(shippAddress._id);
+    address = existedAddress;
+  } else {
+    address = new Address(shippAddress);
+    address.user = user;
+    await address.save();
+
+    user.addresses.push(address);
+    await user.save();
+  }
+
+  const cart = await cartService.findUserCart(user._id);
+  const orderItems = [];
+
+  for (const item of cart.cartItems) {
+    const orderItem = new OrderItem({
+      price: item.price,
+      product: item.product,
+      quantity: item.quantity,
+      size: item.size,
+      userId: item.userId,
+      discountedPrice: item.discountedPrice,
+    });
+
+    const createdOrderItem = await orderItem.save();
+    orderItems.push(createdOrderItem);
+  }
+
+  const createdOrder = new Order({
+    user,
+    orderItems,
+    totalPrice: cart.totalPrice,
+    totalDiscountedPrice: cart.totalDiscountedPrice,
+    discounte: cart.discounte,
+    totalItem: cart.totalItem,
+    shippingAddress: address,
+   
+  });
+
+  const savedOrder = await createdOrder.save();
+
+  return savedOrder;
+}
+
+async function placedOrder(orderId) {
+  const order = await findOrderById(orderId);
+  order.orderStatus = "PLACED";
+  order.paymentDetails.status = "COMPLETED";
+  return await order.save();
+}
+
+async function confirmedOrder(orderId) {
+  const order = await findOrderById(orderId);
+  order.orderStatus = "CONFIRMED";
+  return await order.save();
+}
+
+async function shipOrder(orderId) {
+  const order = await findOrderById(orderId);
+  order.orderStatus = "SHIPPED";
+  return await order.save();
+}
+
+async function deliveredOrder(orderId) {
+  const order = await findOrderById(orderId);
+  order.orderStatus = "DELIVERED";
+  return await order.save();
+}
+
+async function cancelledOrder(orderId) {
+  const order = await findOrderById(orderId);
+  order.orderStatus = "CANCELLED"; // Assuming OrderStatus is a string enum or a valid string value
+  return await order.save();
+}
+
+
+
+
+module.exports = {
+  createOrder,
+  placedOrder,
+  confirmedOrder,
+  shipOrder,
+  deliveredOrder,
+  cancelledOrder
+};
