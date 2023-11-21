6ffea0147e8f0d44c3197de71591d5cba8a6bba7
Harish1611 <harishnaidub005@gmail.com>
2023-11-21 17:55:45 +0530

    New: Created Product Admin Router layer with admin Product  api routes

---
+++ src/routes/product.admin.routes.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const router=express.Router();
+const productController=require("../controller/product.controller.js");
+const authenticate = require("../middleware/authenticate.js");
+
+
+router.post('/',authenticate, productController.createProduct);
+router.post('/creates',authenticate, productController.createMultipleProduct);
+router.delete('/:id',authenticate, productController.deleteProduct);
+router.put('/:id',authenticate, productController.updateProduct);
+
+module.exports=router;
