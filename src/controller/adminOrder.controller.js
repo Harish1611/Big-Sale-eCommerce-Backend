85e1960fed75c297c813efdca4c9a4e5aff1bf31
Harish1611 <harishnaidub005@gmail.com>
2023-11-21 17:00:41 +0530

    Extended: Added deliverOrder, cancelledOrder, deleteOrder controller functions in adminOrder controller layer

--- src/controller/adminOrder.controller.js
+++ src/controller/adminOrder.controller.js
@@ -31,9 +31,43 @@ const shippOrder = (req, res) => {
 };
 
 
-
+const deliverOrder = (req, res) => {
+    try {
+      const orderId = req.params.orderId;
+      const order = orderService.deliveredOrder(orderId);
+      return res.status(202).send(order);
+    } catch (error) {
+      return res.status(500).json({ error: "Something went wrong" });
+    }
+  };
+  
+  const cancelledOrder = (req, res) => {
+    try {
+      const orderId = req.params.orderId;
+      const order = orderService.cancelledOrder(orderId);
+      return res.status(202).send(order);
+    } catch (error) {
+      return res.status(500).json({ error: "Something went wrong" });
+    }
+  };
+  
+  const deleteOrder = (req, res) => {
+    try {
+      const orderId = req.params.orderId;
+      orderService.deleteOrder(orderId);
+      res
+        .status(202)
+        .json({ message: "Order Deleted Successfully", success: true });
+    } catch (error) {
+      res.status(500).json({ error: "Something went wrong" });
+    }
+  };
+  
 
 module.exports = {
     getAllOrders,
     confirmedOrder,
-    shippOrder,}
+    shippOrder,
+    deliverOrder,
+    cancelledOrder,
+    deleteOrder}
