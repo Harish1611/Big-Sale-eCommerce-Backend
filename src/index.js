ee0d35a4e94d32ed2d9b107ddf38476d4d460084
Harish1611 <harishnaidub005@gmail.com>
2023-11-21 18:08:16 +0530

    New: Added final api routes with endpoints in index.js file

--- src/index.js
+++ src/index.js
@@ -17,6 +17,29 @@ const userRouters = require("./routes/user.routes.js");
 app.use("/api/users",userRouters);
 
 
+const productRouter=require("./routes/product.routes.js");
+app.use("/api/products",productRouter);
+
+const adminProductRouter=require("./routes/product.admin.routes.js");
+app.use("/api/admin/products",adminProductRouter);
+
+const cartRouter=require("./routes/cart.routes.js")
+app.use("/api/cart", cartRouter);
+
+const cartItemRouter=require("./routes/cartItem.routes.js")
+app.use("/api/cart_items",cartItemRouter);
+
+const orderRouter=require("./routes/order.routes.js");
+app.use("/api/orders",orderRouter);
+
+const reviewRouter=require("./routes/review.routes.js");
+app.use("/api/reviews",reviewRouter);
+
+const ratingRouter=require("./routes/rating.routes.js");
+app.use("/api/ratings",ratingRouter);
+
+const adminOrderRoutes=require("./routes/adminOrder.routes.js");
+app.use("/api/admin/orders",adminOrderRoutes);
 
 module.exports = app;
 
