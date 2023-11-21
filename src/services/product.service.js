84ce9ea6c55ce64c9dd22383fbed15bedcb6d9cf
Harish1611 <harishnaidub005@gmail.com>
2023-11-21 16:39:00 +0530

    Extended: Created getAllProducts functions with Filtering & Pagination logic

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -92,9 +92,85 @@ async function deleteProduct(productId) {
     return product;
   }
 
+  
+// Get all products with filtering and pagination
+async function getAllProducts(reqQuery) {
+    let {
+      category,
+      color,
+      sizes,
+      minPrice,
+      maxPrice,
+      minDiscount,
+      sort,
+      stock,
+      pageNumber,
+      pageSize,
+    } = reqQuery;
+    (pageSize = pageSize || 10), (pageNumber = pageNumber || 1);
+    let query = Product.find().populate("category");
+  
+  
+    if (category) {
+      const existCategory = await Category.findOne({ name: category });
+      if (existCategory)
+        query = query.where("category").equals(existCategory._id);
+      else return { content: [], currentPage: 1, totalPages:1 };
+    }
+  
+    if (color) {
+      const colorSet = new Set(color.split(",").map(color => color.trim().toLowerCase()));
+      const colorRegex = colorSet.size > 0 ? new RegExp([...colorSet].join("|"), "i") : null;
+      query = query.where("color").regex(colorRegex);
+      // query = query.where("color").in([...colorSet]);
+    }
+  
+    if (sizes) {
+      const sizesSet = new Set(sizes);
+      
+      query = query.where("sizes.name").in([...sizesSet]);
+    }
+  
+    if (minPrice && maxPrice) {
+      query = query.where("discountedPrice").gte(minPrice).lte(maxPrice);
+    }
+  
+    if (minDiscount) {
+      query = query.where("discountPersent").gt(minDiscount);
+    }
+  
+    if (stock) {
+      if (stock === "in_stock") {
+        query = query.where("quantity").gt(0);
+      } else if (stock === "out_of_stock") {
+        query = query.where("quantity").lte(0);
+      }
+    }
+  
+    if (sort) {
+      const sortDirection = sort === "price_high" ? -1 : 1;
+      query = query.sort({ discountedPrice: sortDirection });
+    }
+  
+    // Apply pagination
+    const totalProducts = await Product.countDocuments(query);
+  
+    const skip = (pageNumber - 1) * pageSize;
+  
+    query = query.skip(skip).limit(pageSize);
+  
+    const products = await query.exec();
+  
+    const totalPages = Math.ceil(totalProducts / pageSize);
+  
+  
+    return { content: products, currentPage: pageNumber, totalPages:totalPages };
+  }
+
+
   module.exports = {
     createProduct,
     deleteProduct,
     updateProduct,
-    getAllProducts,
-    findProductById,}
+    findProductById,
+    getAllProducts}
