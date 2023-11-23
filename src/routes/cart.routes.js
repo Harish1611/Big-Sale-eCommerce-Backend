db53564a7570b150b2447debba1697a30fdac569
Harish1611 <harishnaidub005@gmail.com>
2023-11-23 10:23:03 +0530

    API Test: PUT API Route of Add to Cart Tested - Working Properly

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -4,10 +4,10 @@ const router=express.Router();
 const cartController=require("../controller/cart.controller.js")
 
 // GET: /api/cart
-router.get("/", authenticate, cartController.findUserCart);  //Tested Working
+router.get("/", authenticate, cartController.findUserCart);   // Tested:Wroking
 
 // PUT: /api/cart/add
-router.put("/add", authenticate, cartController.addItemToCart);
+router.put("/add", authenticate, cartController.addItemToCart);  // Tested:Wroking
 
 
 module.exports=router;
