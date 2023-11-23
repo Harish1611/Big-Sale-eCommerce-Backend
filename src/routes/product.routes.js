13c48244f5b104b782632534bbaa9ea12b7f14b8
Harish1611 <harishnaidub005@gmail.com>
2023-11-23 11:16:06 +0530

    API Test: Find Product By ID GET API Route Tested - Working Properly

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,7 +4,7 @@ const productController=require("../controller/product.controller.js");
 const authenticate = require("../middleware/aunthenticate.js");
 
 router.get('/',authenticate, productController.getAllProducts);
-router.get('/id/:id',authenticate, productController.findProductById);
+router.get('/id/:id',authenticate, productController.findProductById); // Tested: Working 
 
 
 module.exports = router;
