f3f3cc8c341b979394000ed8500010a33f59fecf
Harish1611 <harishnaidub005@gmail.com>
2023-11-23 11:19:20 +0530

    API Test: Get All Products GET API Route Tested - Working Properly

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const productController=require("../controller/product.controller.js");
 const authenticate = require("../middleware/aunthenticate.js");
 
-router.get('/',authenticate, productController.getAllProducts);
+router.get('/',authenticate, productController.getAllProducts); // Tested: Working 
 router.get('/id/:id',authenticate, productController.findProductById); // Tested: Working 
 
 
