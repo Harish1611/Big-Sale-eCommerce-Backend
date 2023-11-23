6de84224cd2cfa23cba9ac1a9e9805b4d997de3b
Harish1611 <harishnaidub005@gmail.com>
2023-11-23 10:16:35 +0530

    API Test: GET Cart api route is tested- working properly

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -4,7 +4,7 @@ const router=express.Router();
 const cartController=require("../controller/cart.controller.js")
 
 // GET: /api/cart
-router.get("/", authenticate, cartController.findUserCart);
+router.get("/", authenticate, cartController.findUserCart);  //Tested Working
 
 // PUT: /api/cart/add
 router.put("/add", authenticate, cartController.addItemToCart);
