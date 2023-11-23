e1509315b22641eabfefcb3c6674c58e4d3e8fe5
Harish1611 <harishnaidub005@gmail.com>
2023-11-23 11:07:14 +0530

    API Test: Update Cart Item PUT API Route Tested - Working Properly

--- src/routes/cartItem.routes.js
+++ src/routes/cartItem.routes.js
@@ -4,7 +4,7 @@ const router=express.Router();
 
 const cartItemController=require("../controller/cartItem.controller.js");
 
-router.put("/:id",authenticate,cartItemController.updateCartItem);
+router.put("/:id",authenticate,cartItemController.updateCartItem); // Tested: Working
 router.delete("/:id",authenticate,cartItemController.removeCartItem); //Tested: Wroking
 
 module.exports=router;
