22c9902fe73ac9d0e92f64d150aa27a4a65700b8
Harish1611 <harishnaidub005@gmail.com>
2023-11-23 10:25:35 +0530

    API Test: GET Profile API Route Tested - Working Properly

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controller/user.controller.js");
 
 
-router.get("/profile",userController.getUserProfile);
+router.get("/profile",userController.getUserProfile); // Tested: Working 
 router.get("/", userController.getAllUsers);
 
 module.exports = router;
